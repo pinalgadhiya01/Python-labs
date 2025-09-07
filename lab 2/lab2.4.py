c7a7ed7d8a93adaade89deed2aa2af8e2e6976db
pinalgadhiya01 <pinalgadhiya0108@gmail.com>
2025-09-07 10:01:40 +0530

    Add files via upload

---
+++ lab 2/lab2.4.py
@@ -0,0 +1,6 @@
+x = float(input("Enter value of x: "))
+y = float(input("Enter value of y: "))
+
+z = (x + y) * (x - y)
+
+print("The value of z is:", z)
---
+++ lab 2/lab2.5.py
@@ -0,0 +1,7 @@
+x = float(input("Enter value of x: "))
+y = float(input("Enter value of y: "))
+
+# Calculate z using the equation: z = (x + y)^2 - 2xy
+z = (x + y)**2 - 2 * x * y
+
+print("The value of z=(x+y)^2-2xy is:", z)
