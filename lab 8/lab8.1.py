f1722c43e0de1e5fd6883954b2fe36ffa1aadfd6
pinalgadhiya01 <pinalgadhiya0108@gmail.com>
2025-09-07 11:12:17 +0530

    Add files via upload

---
+++ lab 8/lab8.1.py
@@ -0,0 +1,7 @@
+import math
+
+degree = float(input("Enter angle in degrees: "))
+
+radian = math.radians(degree)
+
+print(f"{degree} degrees is equal to {radian} radians")
---
+++ lab 8/lab8.3.py
@@ -0,0 +1,12 @@
+import math
+x=math.pi
+
+f = math.cos(2 * x)
+f1 = -2 * math.sin(2 * x)
+f2 = -4 * math.cos(2 * x)
+
+
+
+print("f(x) =", f)
+print("f'(x) =", f1)
+print("f''(x) =", f2)
